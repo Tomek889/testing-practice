9f52110075d22bdf0e2cc7b845ab5c5336daccdf
Tomasz Styczeń <97025562+Tomek889@users.noreply.github.com>
2025-05-08 13:05:12 +0200

    Create reverseString functiona and test it

--- functions.js
+++ functions.js
@@ -2,6 +2,15 @@ function capitalize(text) {
   return text.trim().toUpperCase()[0];
 }
 
+function reverseString(text) {
+  let reversed = "";
+  for (let i = 0; i < text.length; i++) {
+    reversed += text[text.length - 1 - i];
+  }
+  return reversed;
+}
+
 module.exports = {
   capitalize,
+  reverseString,
 };
--- functions.test.js
+++ functions.test.js
@@ -1,5 +1,6 @@
-const { capitalize } = require("./functions.js");
+const { capitalize, reverseString } = require("./functions.js");
 
+// capitalize test
 test("capitalize from lowercase (test 1)", () => {
   expect(capitalize("t")).toBe("T");
 });
@@ -14,4 +15,17 @@ test("capitalize from lowercase (test 3)", () => {
 
 test("capitalize from lowercase (test 4)", () => {
   expect(capitalize("")).toBeUndefined();
+});
+
+// reverseString test
+test("reverses a string (test 1)", () => {
+  expect(reverseString("reversed")).toBe("desrever");
+});
+
+test("reverses a string (test 2)", () => {
+  expect(reverseString("ko ko")).toBe("ok ok");
+});
+
+test("reverses a string (test 3)", () => {
+  expect(reverseString("12345")).toBe("54321");
 });
